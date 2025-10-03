2d9d3b1246002938d6f972b3f5eb7e117ee5dd8b
Cristian Ramirez <cristiandramirezm.2024@gmail.com>
2025-10-03 06:54:43 -0500

    project creationg

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_facade_transfer
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/uranodev/flutter_facade_transfer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.uranodev.flutter_facade_transfer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
